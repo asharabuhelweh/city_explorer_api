1cdfceef4e716e75f5c3dbe0162f56b6944914c3
asharabuhelwh <ashar.mohamad@yahoo.com>
2021-04-07 18:54:24 +0300

    done

--- server.js
+++ server.js
@@ -9,15 +9,18 @@ const superagent = require('superagent');
 const PORT = process.env.PORT || 5000;
 const client = new pg.Client({
   connectionString: process.env.DATABASE_URL,
-   ssl:
-    {
-       rejectUnauthorized: false }
+  //  ssl:
+  //   {
+  //      rejectUnauthorized: false }
 });
 server.use(cors());
 server.get('/', homeRouteHandler);
 server.get('/location', locationHandler);
 server.get('/weather', weatherHandler);
 server.get('/parks', parkHandler);
+server.get('/movies', movieHandler);
+server.get('/yelp',yelpHandler);
+
 server.get('*', errorHandler);
 
 // request url (browser): localhost:3000/
@@ -94,6 +97,51 @@ function parkHandler(req, res) {
       res.send(data2);
     });
 }
+`https://api.themoviedb.org/3/movie/550?api_key=01cf2f200ad09f9b1fdfcf84a43fc9de
+`
+function movieHandler(req, res) {
+  // console.log(req.query);
+  let data3 = [];
+  let movieName = req.query.search_query;
+  console.log(movieName);
+  let key = process.env.MOVIE_KEY;
+  // `https://api.moviebit.io/v2.0/forecast/daily`
+  let movieURL=`https://api.themoviedb.org/3/search/movie?api_key=${key}&query=${movieName}`;
+  superagent.get(movieURL)
+    .then(result => {
+      // console.log(result.body.data.movie);
+      result.body.results.map(val => {
+        data3.push(new Movie (val));
+      });
+      res.send(data3);
+    });
+}
+let start =1;
+
+function yelpHandler(req, res) {
+  // console.log(req.query);
+  let data4 = [];
+
+  let yelpName = req.query.search_query;
+  let key = process.env.YELP_KEY;
+let page =req.query.page;
+  let numPerPage=5;
+  let start=((page-1)* numPerPage +1);
+  console.log(yelpName);
+  // `https://api.yelp.com/v3/businesses/search`
+  let yelpURL=`https://api.yelp.com/v3/businesses/search?location=${yelpName}&limit=${numPerPage}&offest=${start}`;
+  superagent.get(yelpURL).set('authorization',`Bearer ${key}`)
+
+
+    .then(result => {
+      // console.log(result.body.data.yelp);
+      result.body.businesses.map(val => {
+        data4.push(new Yelp (val));
+      });
+      res.send(data4);
+    });
+}
+
 function Location (cityName,geoData) {
   this.search_query = cityName;
   this.formatted_query = geoData[0].display_name;
@@ -102,8 +150,8 @@ function Location (cityName,geoData) {
 }
 function Weather(weatherDay) {
   console.log(weatherDay);
-  this.description = weatherDay.weather.description;
-  this.valid_date = weatherDay.valid_date;
+  this.forecast = weatherDay.weather.description;
+  this.time = weatherDay.valid_date;
 }
 function Park(parkData) {
   this.name = parkData.name;
@@ -112,6 +160,26 @@ function Park(parkData) {
   this.description = parkData.description;
   this.url = parkData.url;
 }
+function Movie(movieData) {
+  this.title = movieData.title;
+  this.overview = movieData.overview;
+  this.average_votes = movieData.vote_average;
+  this.total_votes = movieData.vote_count;
+  this.image_url =`https://image.tmdb.org/t/p/w500/${movieData.poster_path}`;
+  this.popularity = movieData.popularity;
+  this.released_on = movieData.release_date;
+}
+
+
+function Yelp(yelpData) {
+  this.name = yelpData.name;
+  this.image_url = yelpData.image_url;
+  this.price= yelpData.price;
+  this.rating = yelpData.rating;
+  this.url = yelpData.url;
+  
+}
+
 //location:3030/ddddddd
 function errorHandler(req, res) {
   let errObj = {
